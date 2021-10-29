751a80aad1d0b52fceb26014ef4abfa43abf8599
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-29 08:05:33 -0800

    Change contains1() to containsX to dry up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function contains1(int){
+function containsX(int, X){
   intArray = (int).toString().split("").map(Number); // turn the int into a string so we can split, map(Number) will loop through and change them to a number, parseInt() does not work in this instance
   
   for(const int of intArray){
-    if(int === 1){
+    if(int === X){
       return true;
     } 
   }
@@ -15,9 +15,9 @@ function beepBoopify(int){
   let outputArray = [];
   for (let i = 0; i <= int; i++)
   {
-    if(contains1(i)){
+    if(contains1(i, 1)){
       outputArray.push("Beep!");
-    } else if(i === 2){
+    } else if(containsX(i, 2)){
       outputArray.push("Boop!");
     }
     else{
